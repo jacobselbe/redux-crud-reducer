26680dfeb84a6482f4f7a43502e917a402aaa393
Jacob Selbe <jalxselbe@gmail.com>
2019-05-27 18:34:22 -0500

    removed component to focus first on logging state with dispatch only

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from '../node_modules/react';
 import ReactDOM from '../node_modules/react-dom';
 import {Provider} from 'react-redux';
-import Board from './components/board';
-import store from './store'; 
+import store from './store';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Board />
-    </Provider>,
-    document.getElementById('root')
-);
+import { addItem, updateItem, deleteItem } from './actions';
+
+store.dispatch(addItem({ id: 0, name: 'Foo' }));
+store.dispatch(addItem({ id: 1, name: 'Bar' }));
+store.dispatch(addItem({ id: 2, name: 'Baz' }));
+
+store.dispatch(updateItem({ id: 0, name: 'Food' }));
+store.dispatch(updateItem({ id: 1, name: 'Bard' }));
+store.dispatch(updateItem({ id: 2, name: 'Bash' }));
+
+store.dispatch(deleteItem({ id: 1 }));
+
+// We expect to see:
+// [
+//     {
+//         id: 0,
+//         name: 'Food'
+//     },
+//     {
+//         id: 2,
+//         name: 'Bash'
+//     }
+// ]
+console.log(store.getState());
