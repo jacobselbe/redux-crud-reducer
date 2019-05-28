c0f8328691f5174930480491a3c82ded1a9143fd
Jacob Selbe <jalxselbe@gmail.com>
2019-05-27 20:41:24 -0500

    updated reducers to correctly log dispatch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,26 @@ const initialState = {
 };
 
 export const crudReducer = (state = initialState, action) => {
-    // Add code which handles each action here
     if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            items: [...state.items, action.item]
+        });
     }
     else if (action.type === UPDATE_ITEM) {
+        const updatedItems = state.items.map(item => 
+            item.id === action.item.id ? action.item : item
+        );
+        return Object.assign({}, state, {
+            items: updatedItems
+        });
     }
     else if (action.type === DELETE_ITEM) {
+        const updatedItems = state.items.filter(item =>
+            item.id !== action.item.id
+        );
+        return Object.assign({}, state, {
+            items: updatedItems
+        });
     }
     return state;
 };
